7536c2e48e8a5dfa906dca7599985966a4e2cf58
guillaume wernert <guil.wernert@gmail.com>
2023-02-03 23:44:24 +0100

    création navBar

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function NavBar() {
+  return (
+    <nav className="h-20 flex ">
+      <Link to="/">
+        <img className="h-16 mr-10" src="\src\assets\logo_fcd.jpg" alt="" />
+      </Link>
+      <Link to="/players">Listes des joueurs</Link>
+      <Link to="/player">Un joueur</Link>
+      <Link to="/Login">Connexion</Link>
+    </nav>
+  );
+}
+
+export default NavBar;
