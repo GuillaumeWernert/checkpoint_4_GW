45300ac8548e3cb8b24c2defb5f8c9f16a4b4d72
guillaume wernert <guil.wernert@gmail.com>
2023-02-03 21:17:58 +0100

    integration footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+      <p className="text-center">© 2023 Copyright - G.W.</p>
+    </div>
+  );
+}
+
+export default Footer;
