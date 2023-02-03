7678137647362fc5c1b65c2a8b69c36d5fba5ec7
guillaume wernert <guil.wernert@gmail.com>
2023-02-03 19:23:25 +0100

    page players dur responsive

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import Players from "@pages/Players";
 import Home from "./pages/Home";
 
 import "./App.css";
@@ -9,6 +10,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="home" element={<Home />} />
+        <Route path="players" element={<Players />} />
       </Routes>
     </Router>
   );
